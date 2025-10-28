15c803e7aadb4c9e8dd5489b1f2704956178b7e1
Adam Musakabantu Muyobo <muyoboadam@gmail.com>
2025-10-28 08:24:21 +0200

    initial commit

---
+++ src/main/java/com/neighbourhoodwatch/api/NeighbourhoodWatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.neighbourhoodwatch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighbourhoodWatchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeighbourhoodWatchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neighbourhoodwatch/api/NeighbourhoodWatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neighbourhoodwatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighbourhoodWatchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
